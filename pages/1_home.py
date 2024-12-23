3e12e6dff2ff4ac0e66f7305bc5413a688f99a73
eman-nawzad <eman.nawzad00@gmail.com>
2024-12-23 22:15:01 +0300

    Update and rename home.py to 1_home.py

---
+++ pages/1_home.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+def display():
+    st.title("Home Page")
+    st.write("Welcome to the LCZ Web App!")
+
--- pages/home.py
+++
@@ -1,7 +0,0 @@
-import streamlit as st
-
-def display():
-    st.title("Welcome to the LCZ Web App!")
-    st.write("This web application provides an interactive map of Local Climate Zones (LCZ) and other related features.")
-    st.image("assets/favicon.png", width=150)
-    st.write("Use the sidebar to navigate between pages.")
