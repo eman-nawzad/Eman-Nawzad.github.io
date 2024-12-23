753029f8acf52b628ff223777d52dfce59d61984
eman-nawzad <eman.nawzad00@gmail.com>
2024-12-23 20:14:05 +0300

    Update and rename home.py to Home.py

---
+++ pages/Home.py
@@ -0,0 +1,17 @@
+import streamlit as st
+
+def display():
+    st.title("Home")
+    st.write("""
+    Welcome to the Local Climate Zones (LCZ) Web Application!
+    
+    This app provides an interactive platform to explore and visualize Local Climate Zones (LCZ).
+    
+    **Features:**
+    - View LCZ data interactively on a map.
+    - Learn about LCZ classifications in the About section.
+    - Contact us for feedback or questions.
+
+    Use the sidebar to navigate between pages.
+    """)
+
