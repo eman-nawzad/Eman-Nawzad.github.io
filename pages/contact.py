8e14fcca0953e239944defd56e65cedb47a25797
eman-nawzad <eman.nawzad00@gmail.com>
2024-12-23 16:00:54 +0300

    Update contact.py

--- pages/contact.py
+++ pages/contact.py
@@ -1,13 +1,1 @@
-def display():
-    import streamlit as st
 
-    st.title("Contact Us")
-    st.markdown(
-        """
-        ### Reach out for inquiries or feedback:
-        - **Email**: your_email@example.com
-        - **GitHub**: [Your Repository](https://github.com/your_username/LCZ-WebApp)
-        - **LinkedIn**: [Your LinkedIn Profile](https://linkedin.com/in/your_profile)
-        """
-    )
-    st.success("We’d love to hear from you!")
